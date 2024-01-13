8e55bf7e38fecf3185004f284111b437329d12a6
Javier Aleman <javiersettex@gmail.com>
2024-01-12 19:48:07 -0500

    initialization

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,21 @@
+import dotenv from 'dotenv';
 import pg from 'pg';
 const { Pool } = pg;
+dotenv.config();
+
+//const around = "pro";
+const around = "dev";
+
+const poolHost =  around==='pro'? process.env.DB_HOST:	'localhost';
+const poolName =  around==='pro'? process.env.DB_HOST:	'localmotosdk';
+const poolUser =  around==='pro'? process.env.DB_HOST:	'postgres';
+const poolPass =  around==='pro'? process.env.DB_HOST:	'0';
+const poolPort =  around==='pro'? process.env.DB_HOST: 	'5432';
+
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'motosdk',
-  password: '0',
-  port: 5432, 
+	host: 		poolHost,
+	database: 	poolName,
+  	user: 		poolUser,
+  	password: 	poolPass,
+  	port: 		poolPort,
 });
